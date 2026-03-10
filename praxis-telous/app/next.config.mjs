1e983feb6c41f6c4c804131d71f5c461f2fa5aeb
ducky <tom.lucette@outlook.com>
2026-03-10 12:53:18 +0000

    gh pages - production fix

---
+++ praxis-telous/app/next.config.mjs
@@ -0,0 +1,14 @@
+const repoName = 'praxis-telous';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
